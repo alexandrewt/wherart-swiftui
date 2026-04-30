9d79d715558d02ab2b4c307d4426d538a01e3ec7
alexandrewt <alexandredewitt@gmail.com>
2026-04-30 10:00:28 +0200

    Initial Commit

---
+++ Wherart/WherartApp.swift
@@ -0,0 +1,17 @@
+//
+//  WherartApp.swift
+//  Wherart
+//
+//  Created by Alexandre de Witt on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct WherartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
